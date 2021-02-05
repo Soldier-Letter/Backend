1a59e227f9913ce73683b4e4215a8439b85469f9
nahyun <nahyun858@gmail.com>
2021-02-05 19:33:25 +0900

    임의데이터

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+
 const bodyParser = require('body-parser');
 
 dotenv.config();
@@ -11,6 +12,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('./routes'));
+
+
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);

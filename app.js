dd46a52b24892a4ed540a9ceed703ea7d9922ffe
uniqueimaginate <uniqueimaginate@gmail.com>
2021-02-06 00:47:16 +0900

    Now Safe

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 // const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+
 const bodyParser = require('body-parser');
 
 dotenv.config();
 const port = process.env.PORT;
 
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('./routes'));
+
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
--- routes/api/div.js
+++ routes/api/div.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const mysqlUtil = require('../../utils/mysqlUtils');
 const router = new express.Router();
+const { auth, hash, emailValidator } = require('../middleware/auth');
+
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.KEYID,
+  secretAccessKey: process.env.KEY,
+  region: process.env.REGION,
+});
+
+let imageName = '';
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    bucket: process.env.BUCKET,
+    key: function (req, file, cb) {
+      imageName = Date.now() + file.originalname;
+      cb(null, imageName);
+    },
+    acl: 'public-read-write',
+  }),
+});
 
 router.get('/div/info', async function (req, res, next) {
   try {
@@ -170,6 +196,147 @@ router.get('/div/local/search', async function (req, res, next) {
     res.status(400).send(e);
   }
 });
+
+router.post('/div/local', auth, async function (req, res, next) {
+  try {
+    const user = req.user;
+    const params = req.body;
+    if (!paramCheck(params, 'div_uid')) {
+      res.status(400).send('div_uid 파라미터 확인');
+    }
+    if (!paramCheck(params, 'name')) {
+      res.status(400).send('name 파라미터 확인');
+    }
+    if (!paramCheck(params, 'address')) {
+      res.status(400).send('address 파라미터 확인');
+    }
+    if (!paramCheck(params, 'content')) {
+      res.status(400).send('content 파라미터 확인');
+    }
+    if (!paramCheck(params, 'type')) {
+      res.status(400).send('type 파라미터 확인');
+    }
+
+    const phoneNumber = params['phone_number'] ? params['phone_number'] : null;
+    const price = params['price'] ? params['price'] : null;
+    const information = params['information'] ? params['information'] : null;
+
+    const divFacility = await mysqlUtil(
+      'call proc_insert_div_local(?,?,?,?,?,?,?,?,?)',
+      [
+        user['uid'],
+        params['div_uid'],
+        params['type'],
+        params['name'],
+        phoneNumber,
+        params['address'],
+        price,
+        params['content'],
+        information,
+      ],
+    );
+    res.status(200).send(divFacility[0]);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
+});
+
+router.post('/div/rating', auth, async function (req, res, next) {
+  try {
+    const user = req.user;
+    const params = req.body;
+    if (!paramCheck(params, 'div_uid')) {
+      res.status(400).send('div_uid 파라미터 확인');
+    }
+    if (!paramCheck(params, 'rating')) {
+      res.status(400).send('rating 파라미터 확인');
+    }
+    if (!paramCheck(params, 'px_rating')) {
+      res.status(400).send('px_rating 파라미터 확인');
+    }
+    if (!paramCheck(params, 'domitory_rating')) {
+      res.status(400).send('domitory_rating 파라미터 확인');
+    }
+    if (!paramCheck(params, 'shower_rating')) {
+      res.status(400).send('shower_rating 파라미터 확인');
+    }
+    if (!paramCheck(params, 'meal_rating')) {
+      res.status(400).send('meal_rating 파라미터 확인');
+    }
+    if (!paramCheck(params, 'location_rating')) {
+      res.status(400).send('location_rating 파라미터 확인');
+    }
+    if (!paramCheck(params, 'comment')) {
+      res.status(400).send('comment 파라미터 확인');
+    }
+    if (!paramCheck(params, 'pros')) {
+      res.status(400).send('pros 파라미터 확인');
+    }
+    if (!paramCheck(params, 'cons')) {
+      res.status(400).send('cons 파라미터 확인');
+    }
+    if (!paramCheck(params, 'content')) {
+      res.status(400).send('content 파라미터 확인');
+    }
+    const ratingInfo = await mysqlUtil(
+      'call proc_insert_div_rating(?,?,?,?,?,?,?,?,?,?,?,?)',
+      [
+        user['uid'],
+        params['div_uid'],
+        params['rating'],
+        params['px_rating'],
+        params['domitory_rating'],
+        params['shower_rating'],
+        params['meal_rating'],
+        params['location_rating'],
+        params['comment'],
+        params['pros'],
+        params['cons'],
+        params['content'],
+      ],
+    );
+
+    const divInfo = await mysqlUtil('call proc_update_div_rating(?)', [
+      params['div_uid'],
+    ]);
+    const item = {
+      rating: ratingInfo[0],
+      division: divInfo[0],
+    };
+    res.status(200).send(item);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
+});
+
+router.post(
+  '/div/local/image',
+  [auth, upload.single('image')],
+  async function (req, res, next) {
+    try {
+      const params = req.body;
+      if (!paramCheck(params, 'uid')) {
+        res.status(400).send('uid 파라미터 확인');
+      }
+      if (!req.file) {
+        res.status(400).send('이미지 파일 확인');
+      }
+      const divLocalInfo = await mysqlUtil(
+        'call proc_update_div_local_image(?,?)',
+        [
+          params['uid'],
+          `https://hackathonbootcamp.s3.ap-northeast-2.amazonaws.com/${imageName}`,
+        ],
+      );
+      res.status(200).send(divLocalInfo[0]);
+    } catch (e) {
+      console.log(e);
+      res.status(400).send(e);
+    }
+  },
+);
 
 // eslint-disable-next-line require-jsdoc
 function paramCheck(params, key) {

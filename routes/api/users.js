22e283ce8fd8f5fc0af0644e2691d940a033ef16
Donguk Lee <uniqueimaginate@gmail.com>
2021-04-21 23:55:32 +0900

    Erase unnecessary codes

--- routes/api/users.js
+++ routes/api/users.js
@@ -56,17 +56,6 @@ router.post('/user', [emailValidator, hash], async (req, res) => {
     if (!paramUtil.paramCheck(user, 'brigade')) {
       res.status(400).send('keyword 파라미터 확인');
     }
-    // if (
-    //   !req.body.email ||
-    //   !req.body.password ||
-    //   !req.body.name ||
-    //   !req.body.status ||
-    //   !req.body.position ||
-    //   !req.body.division ||
-    //   !req.body.brigade
-    // ) {
-    //   return res.status(400).send('모든 파라미터를 입력받지 못했습니다.');
-    // }
 
     const [isUser] = await mysqlUtils('CALL proc_select_user_exist(?)', [
       user.email,
